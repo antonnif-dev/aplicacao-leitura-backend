88f167f06dd13f5653ff14cb16ab12387f39deec
antonnif-dev <antonnidev@gmail.com>
2025-10-27 14:58:27 -0300

    conectando site na vercel ao backend

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,27 @@ import { errorHandler } from "./middlewares/errorHandler.js";
 dotenv.config();
 
 const app = express();
+
+const allowedOrigins = [
+    'https://aplicacao-leitura.vercel.app',
+    'http://localhost:5173'
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // Permite requisições sem 'origin' (como Postman ou apps mobile) OU se a origem está na lista
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'] // Cabeçalhos permitidos
+}));
+
 app.use(express.json());
+
 app.use(cors());
 
 app.use('/api', taskRoutes);
